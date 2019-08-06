bcd259cda38f281d0b4593601597a74406956bbb
davidfunderburk <dafun2@morgan.edu>
2019-08-06 09:57:25 -0400

    updates

--- src/main/java/hbcu/stay/ready/algor/Solution.java
+++ src/main/java/hbcu/stay/ready/algor/Solution.java
@@ -1,8 +1,47 @@
 package hbcu.stay.ready.algor;
+import java.until.*;
 
 public class Solution {
+    Map<String, Integer> magezineMap;
+    Map<String, Integer> noteMap;
 
-    public Boolean harmlessRansomNote(String message , String magazineText){
-        return null;
+
+    public Solution(String magazine, String note) {
+        Collection<String> magazineText = Arrays.aslist(magazine.split(" "));
+        Collection<String> noteText = Arrays.asList(note.split(" "));
+        magazineMap = new HashMap<String, Integer>();
+        noteMap = new HashMap<String, Integer>();
+
+        for (String value : magazineText) {
+            if (magazineMap.get(value) == null) {
+                magazineMap.put(value, 0);
+            }
+            Integer currentValue = magazineMap.get(value);
+            currentValue++;
+            magazineMap.put(value, currentValue);
+        }
+
+        for (String value : magazineText) {
+            if (noteMap.get(value) == null) {
+                noteMap.put(value, 0);
+            }
+            Integer currentValue = noteMap.get(value);
+            currentValue++;
+            noteMap.put(value, currentValue);
+
+        }
+    }
+    public boolean solve (){
+        for(String ransomWord  : noteMap.keySet()){
+            Integer ransomValue = noteMap.get(ransomWord);
+            Integer magazineValue =   magazineMap.get(ransomWord);
+        }
     }
 }
+
+
+
+    //public Boolean harmlessRansomNote(String message , String magazineText){
+        //return null;
+   // }
+//}
